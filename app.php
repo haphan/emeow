c90a46c5c57e3d9ebcdb717ebf38a740dff3999a
haphan <thanhha.work@gmail.com>
2020-04-10 13:09:16 +0800

    Added app.php

---
+++ app.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Emeow\NetworkManager;
+use Emeow\EmailManager;
+use Emeow\Command\EmeowCommand;
+
+$app = new Application('emeow', '1.0.0');
+
+$app->add(new EmeowCommand(null, new NetworkManager(), new EmailManager()))
+    ->getApplication()
+    ->setDefaultCommand('emeow', true)
+    ->run();
